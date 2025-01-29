ca250776d37efd92000f90e36610902d80a96890
Ajay Soni <ajaypsoni1509@gmail.com>
2025-01-29 16:51:56 +0530

    adding day6 assingment

--- Assingment/day5/test2.py
+++ Assingment/day5/test2.py
@@ -4,22 +4,22 @@ import plotly.express as px
 import pandas as pd
 
 data1 = { 'A': [1, 2, 3, 4, 5], 'B': [5, 4, 3, 2, 1], 'C': [2, 3, 4, 5, 6], 'D': [5, 6, 7, 8, 9] }
-df=pd.DataFrame(data1)
-df.to_csv("sample_data.csv",index=False)
-corr_matrix=df.corr()
+df = pd.DataFrame(data1)
+df.to_csv("test2.csv",index=False)
+corr_matrix = df.corr()
 print(corr_matrix)
 
 # Plot the heatmap of the correlation matrix using seaborn
 plt.figure(figsize=(8,6))
 sns.heatmap(corr_matrix,annot=True,cmap='coolwarm',linewidths=0.5)
-plt.title('Correaltion Heatmap')
+plt.title('Correlation Heatmap')
 plt.show()
 
 # Plot the scatter plot for the two highest correlated columns using matplotlib and plotly  Find the two highest correlated columns
-corr_pairs=corr_matrix.unstack()
-sorted_pairs=corr_pairs.sort_values(kind="quicksort",ascending=False)
-highest_corr=sorted_pairs[1:2]
-col1,col2=highest_corr.index[0]
+corr_pairs = corr_matrix.unstack()
+sorted_pairs = corr_pairs.sort_values(kind='quicksort',ascending=False)
+highest_corr = sorted_pairs[1:2]
+col1,col2 = highest_corr.index[0]
 
 # Scatter plot using matplotlib
 plt.figure(figsize=(8,6))

5db1b50f88c00b926b07e5d5c0f4da003de1419a
Ajay Soni <ajaypsoni1509@gmail.com>
2025-01-29 17:07:46 +0530

    changes

--- Assingment/day5/task2.py
+++
@@ -1,7 +0,0 @@
-#  Write a NumPy program to convert a PIL Image into a NumPy array. Also convert
-#    a NumPy array to an image. Display the image.
-
-# Sample Output:
-# [[[255 255 255 0]
-# .......
-# [255 255 255 0]]]
